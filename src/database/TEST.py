7e07f02fbfb89044acc27e4db2fcc81d86649159
hooniegit <130134750+hooniegit@users.noreply.github.com>
2023-07-11 12:49:20 +0900

    fixed lib realted with mysql

--- src/database/TEST.py
+++ src/database/TEST.py
@@ -1,11 +1,20 @@
 import csv
 
-data = []
-PATH = '/Users/kimdohoon/git/airflow-cordinator-/src/database/pipe_league.csv'
+# data = []
+# PATH = '/Users/kimdohoon/git/airflow-cordinator-/src/database/pipe_league.csv'
+#
+# with open(PATH, 'r') as file:
+#     csv_reader = csv.reader(file)
+#     for row in csv_reader:
+#         data.append(row)
+#
+# print(data)
+
+PATH = '/Users/kimdohoon/src/google/scout_sql.csv'
 
 with open(PATH, 'r') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
-        data.append(row)
+        datas = row
 
-print(data)
+print(datas)
